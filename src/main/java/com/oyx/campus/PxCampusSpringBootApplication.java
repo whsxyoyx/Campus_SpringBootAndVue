42a77c66c43c0b7ffa94b8f5395cbf39bb18049b
xuan <13237107290@163.com>
2023-07-20 09:49:46 +0800

    dev_commite1

--- src/main/java/com/oyx/campus/PxCampusSpringBootApplication.java
+++ src/main/java/com/oyx/campus/PxCampusSpringBootApplication.java
@@ -3,13 +3,16 @@ package com.oyx.campus;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @MapperScan(basePackages = "com.oyx.campus.mapper")
 @SpringBootApplication
 public class PxCampusSpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PxCampusSpringBootApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(PxCampusSpringBootApplication.class, args);
+        String property = context.getEnvironment().getProperty("server.port");
+        System.out.println(property);
     }
 
 }
